6b2836519a2b52199d6ea48c5589b17417d8f2e8
Ks7dev <developer.ks7@gmail.com>
2026-06-13 14:50:36 -0300

    inicio da script (função consulta api)

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-Consumo_de_APIs_Pública_rick_and_morty
+let idAtual = 1
+
+
+let id = document.getElementById("procuta_id")
+document.getElementById("img-person")
+document.getElementById("person-name")
+document.getElementById("person-especie")
+document.getElementById("person-location")
+document.getElementById("gener")
+
+document.addEventListener("butao-anterior")
+document.addEventListener("butao-proximo")
+
+async function consulta-api(params) {
+fetch `https://rickandmortyapi.com/api/character/${id}`
+const 
+
+
+}
