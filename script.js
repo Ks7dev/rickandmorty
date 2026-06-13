4431ad88dbd19e4b35c6ef3bfa3be7b82cd0477f
Ks7dev <developer.ks7@gmail.com>
2026-06-13 16:13:10 -0300

    alterações na estutura

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 let idAtual = 1
+let id = 1
+const urlBase = `https://rickandmortyapi.com/api/character/`
+const urlname = `?name=`
 
+let procuraPerson = document.getElementById("procura_id")
 
-let id = document.getElementById("procuta_id")
-document.getElementById("img-person")
-document.getElementById("person-name")
-document.getElementById("person-especie")
-document.getElementById("person-location")
-document.getElementById("gener")
+ /*object.img*/ = document.getElementById("img-person")
+ /*object.name*/ = document.getElementById("person-name")
+ /*object.especie*/ = document.getElementById("person-especie")
+ /*object.location*/ = document.getElementById("person-location")
+ /*object.gener*/ = document.getElementById("person-gener")
+ /*object.type*/ = document.getElementById("person-type")
 
-document.addEventListener("butao-anterior")
-document.addEventListener("butao-proximo")
+    document.addEventListener("butao-anterior").${id}++
+    document.addEventListener("butao-proximo").${id} -1
 
-async function consulta-api(params) {
-fetch `https://rickandmortyapi.com/api/character/${id}`
-const 
-
-
+ event.key
+if (isNaN(procuraPerson)){
+    async function consultanome(procuraPerson){
+        fetch = `${urlBase}${urlname}${procuraPerson}`}
+        Response.json => {const infoObjetct}
+        console.log(infoObjetct)
+    }
+    }
+} else{
+    async function consulta-id(procuraPerson){
+        fetch = `${urlBase}${procuraPerson}`
+        await id = info = JSON
+    }
 }
